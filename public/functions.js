0b67b7db5699a62ceaa908e04f8ca346b8ff92c9
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2021-11-29 01:36:11 -0600

    first js added to public

--- public/functions.js
+++ public/functions.js
@@ -6,11 +6,22 @@ const logo = document.getElementById('logo');
 const input = document.getElementById('input');
 const findButton = document.getElementById('find-button')
 
+const graph = document.getElementById('grafica')
+const actual = document.getElementById('actual')
+const optima = document.getElementById('optima')
+const promedio = document.getElementById('promedio')
+const contenido = document.getElementById('contenido')
+const tiempo = document.getElementById('tiempo')
+
+const to = document.getElementById('to');
+const from = document.getElementById('from');
+
 // VARIABLES DE CONTROL
 let onCharts = false;
 let apiEndpoint = "https://code-med-iot.herokuapp.com/api/contenedores/";
 let reads = [];
 let id = "";
+let lastRead = [];
 
 // FUNCTIONS
 const goToBeginning = () => {
@@ -26,6 +37,34 @@ const goToCharts = () => {
     onCharts = true;
     
 }
+const updateUI = (containerData) => {
+    optima.innerHTML = "Temperatura óptima: " + containerData.temperaturaOptima + "C";
+    contenido.innerHTML = "Contenido de la hielera: " + containerData.contenido;
+    to.innerHTML = containerData.to;
+    from.innerHTML = containerData.from;
+
+    let reads = Object.entries(containerData.lecturas);
+    lastRead = reads[reads.length - 1];
+
+    actual.innerHTML = "Temperatura actual: " + lastRead[1].temperatura + "C";
+
+    let date = new Date(lastRead[1].fecha).toLocaleString("es-MX", {timeZone: "CST"});
+    tiempo.innerHTML = "Última actualización a las: " + date;
+
+    let sum = 0;
+    reads.forEach(([key, read]) => {
+        let currentRead = document.createElement('div')
+        currentRead.classList.add('lectura')
+        currentRead.style.height = (read.temperatura * 2.5) + "px";
+        grafica.appendChild(currentRead)
+        sum += read.temperatura;
+    });
+    let average = sum / reads.length;
+    
+    promedio.innerHTML = "El promedio de temperatura es de: " + average.toFixed(2) + "C";
+}
+const updateLastRead = () => {}
+const getLastRead = async () => {}
 const getContainer = async (event) => {
     event.preventDefault();
     id = input.value;
@@ -34,9 +73,9 @@ const getContainer = async (event) => {
     const resContainer = await fetch(endpointDirection);
     const dataContainer = await resContainer.json();
 
-    console.log(dataContainer);
-
     goToCharts();
+
+    updateUI(dataContainer.data);
 }
 
 // EVENT LISTENERS
